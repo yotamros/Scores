c2037267e767baf189dc20b66293bbe1f3c14167
Yotam Rosenbaum <yotamrosen@gmail.com>
2014-10-08 12:25:09 -0500

    Initial Commit

---
+++ Scores/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Scores
+//
+//  Created by Yotam Rosenbaum on 10/8/14.
+//  Copyright (c) 2014 yotam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
